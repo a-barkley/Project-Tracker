0b312846ef074bea15c9332c1fc27122628d6239
Henry Nguyen <hln11244@gmail.com>
2022-06-30 11:37:07 -0700

    Timer interval added

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
-new Date($.now());
+var date = Date($.now());
+var projectFormEl = $("#project-form");
 
 var form = $('form')
+var lead = $('.lead')
 var input = $('input')
+var projectType = $('#projectType')
+var projectName = $('#projectName')
+
+setInterval(function () {
+    var date = Date($.now());
+    lead.text(date)
+}, 1000)
+
 
 var inputValue = input.val()
+
+    .submit(projectType.val();
+projectName.val())
